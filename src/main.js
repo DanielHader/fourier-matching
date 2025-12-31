94f442a5d04154ab024a0aeeefd38f73631fac67
Daniel Hader <dhader96@gmail.com>
2025-12-30 20:29:11 -0600

    slider table

--- src/main.js
+++ src/main.js
@@ -1,53 +1,23 @@
 
-const slider_params = {
-    width: 300,
-    height: 8,
-    color: "rgb(220, 220, 220)",
-    
-    knob_width: 6,
-    knob_height: 16,
-    knob_color: "rgb(180, 210, 255)",
-};
 
-class Slider {
-    constructor(x, y, value=4, min_value=0, max_value=10, tick_interval=1) {
-	this.x = x;
-	this.y = y;
-	
-	this.value = value;
-	this.min_value = min_value;
-	this.max_value = max_value;
-	this.tick_interval = tick_interval;
-    }
+function resize_canvas(canvas) {
+    const dpr = window.devicePixelRatio || 1;
+    console.log(dpr);
+    const buffer_width = canvas.clientWidth * dpr;
+    const buffer_height = canvas.clientHeight * dpr;
 
-    draw(ctx, params) {
-	const ratio = this.value / (this.max_value - this.min_value);
-	ctx.strokeStyle = "black";
-
-	ctx.fillStyle = params.color;
-	ctx.fillRect(this.x, this.y - params.height / 2, params.width, params.height);
-	ctx.strokeRect(this.x, this.y - params.height / 2, params.width, params.height);
-	
-	
-	ctx.fillStyle = params.knob_color;
-	ctx.fillRect(this.x + params.width * ratio - params.knob_width / 2,
-		       this.y - params.knob_height / 2,
-		       params.knob_width,
-		       params.knob_height);
-	ctx.strokeRect(this.x + params.width * ratio - params.knob_width / 2,
-		       this.y - params.knob_height / 2,
-		       params.knob_width,
-		       params.knob_height);
-	
-    }
+    canvas.width = buffer_width;
+    canvas.height = buffer_height;
 }
 
 function init(degree) {
     const canvas = document.getElementById("fourier-canvas");
     const context = canvas.getContext("2d");
-    
-    const slider = new Slider(10, 20);
-    slider.draw(context, slider_params);
+    resize_canvas(canvas);
+
+    canvas.addEventListener("pointerdown", (e) => handle_pointerdown_event(e, canvas));
+    canvas.addEventListener("pointerup",   (e) => handle_pointerup_event(e, canvas));
+    canvas.addEventListener("pointermove", (e) => handle_pointermove_event(e, canvas));
 }
 
 init(1);
