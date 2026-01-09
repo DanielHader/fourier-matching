33f804d7ea551fa080b14bd7a12191f45d37d39d
Daniel Hader <dhader96@gmail.com>
2026-01-09 14:52:39 -0600

    text inputs now update sliders

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 let canvas, context;
 
 const bounds = {
-    x_min: -2,
-    x_max:  2,
-    y_min: -1.2,
-    y_max:  1.2,
+    x_min: -1.4,
+    x_max:  1.4,
+    y_min: -2.2,
+    y_max:  2.2,
 };
 
 const amplitude_sliders = [];
@@ -32,8 +32,8 @@ function affine(x, in_min, in_max, out_min, out_max) {
 function plot() {
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    let y_lower = -1.2;
-    let y_upper = 1.2;
+    let y_lower = bounds.y_min;
+    let y_upper = bounds.y_max;
     const xs = [];
     const ys = [];
     for (let px = 0; px < canvas.width; px++) {
@@ -60,7 +60,7 @@ function plot() {
 
     context.lineWidth = 1;
     for (let x = Math.ceil(bounds.x_min); x <= Math.floor(bounds.x_max); x++) {
-	if (x == 0) context.strokeStyle = "#880000";
+	if (x == 0) context.strokeStyle = "#aa0000";
 	else context.strokeStyle = "#888888";
 	
 	context.beginPath();
@@ -72,7 +72,7 @@ function plot() {
 	context.stroke();
     }
     for (let y = Math.ceil(y_lower); y <= Math.floor(y_upper); y++) {
-	if (y == 0) context.strokeStyle = "#008800";
+	if (y == 0) context.strokeStyle = "#00aa00";
 	else context.strokeStyle = "#888888";
 	    
 	context.beginPath();
@@ -98,16 +98,40 @@ function resize_canvas() {
 function amplitude_slider_event(event, index) {
     const value = Number(event.target.value);
     amplitudes[index] = value;
-    amplitude_inputs[index].value = value;
+
+    if (isNaN(value)) {
+	amplitude_inputs[index].style.background = "#ffaaaa";
+    } else {
+	amplitude_inputs[index].style.background = "#ffffff";
+	if (event.target === amplitude_inputs[index]) {
+	    amplitude_sliders[index].value = value;
+	} else {
+	    amplitude_inputs[index].value = value;
+	}
+    }
+
     plot();
 }
 
 function phase_slider_event(event, index) {
     const value = Number(event.target.value);
     phases[index] = value;
-    phase_inputs[index].value = value;
+    
+    if (isNaN(value)) {
+	phase_inputs[index].style.background = "#ffaaaa";
+    } else {
+	phase_inputs[index].style.background = "#ffffff";
+	if (event.target === phase_inputs[index]) {
+	    phase_sliders[index].value = value;
+	} else {
+	    phase_inputs[index].value = value;
+	}
+    }
+    
     plot();
 }
+
+
 
 function init(degree) {
     canvas = document.getElementById("fourier-canvas");
@@ -117,7 +141,7 @@ function init(degree) {
     for (let i = 1; i <= degree; i++) {
 	const a_label = Object.assign(document.createElement("span"), {
 	    className: "label-span",
-	    textContent: `Coefficient ${i}`,
+	    textContent: `Amplitude ${i}`,
 	});
 
 	const p_label = Object.assign(document.createElement("span"), {
@@ -125,44 +149,48 @@ function init(degree) {
 	    textContent: `Phase ${i}`,
 	});
 
+	const amplitude = Math.round(20.0 * (Math.random() * 2.0 - 1.0)) / 20.0;
+	const phase = Math.round(20.0 * Math.random()) / 20.0;
+	
+	amplitudes.push(amplitude);
 	const a_slider = Object.assign(document.createElement("input"), {
 	    className: "slider",
 	    type: "range",
 	    id: `amplitude-slider-${i}$`,
-	    value: 0.0,
 	    min: -1.0,
 	    max: 1.0,
 	    step: 0.05,
+	    value: amplitude,
 	});
 	a_slider.addEventListener("input", (event) => amplitude_slider_event(event, i));
 	amplitude_sliders[i] = a_slider;
-
+	
+	phases.push(phase);
 	const p_slider = Object.assign(document.createElement("input"), {
 	    className: "slider",
 	    type: "range",
 	    id: `phase-slider-${i}$`,
-	    value: 0.0,
 	    min: 0.0,
 	    max: 1.0,
 	    step: 0.05,
+	    value: phase,
 	});
 	p_slider.addEventListener("input", (event) => phase_slider_event(event, i));
 	phase_sliders[i] = p_slider;
 	
 	const a_input = Object.assign(document.createElement("input"), {
 	    id: `amplitude-input-${i}$`,
-	    value: 0.0,
+	    value: amplitude,
 	});
+	a_input.addEventListener("input", (event) => amplitude_slider_event(event, i));
 	amplitude_inputs[i] = a_input;
 
 	const p_input = Object.assign(document.createElement("input"), {
 	    id: `phase-input-${i}$`,
-	    value: 0.0,
+	    value: phase,
 	});
+	p_input.addEventListener("input", (event) => phase_slider_event(event, i));
 	phase_inputs[i] = p_input;
-
-	amplitudes.push(0);
-	phases.push(0);
 
 	controls.appendChild(a_label);
 	controls.appendChild(a_slider);
